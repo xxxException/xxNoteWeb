70afa87fcb6790db7bfab8e87f5ac567fa4bf275
xxxException <chunhei1997@outlook.com>
2019-10-09 15:17:38 +0800

    sql

--- main.go
+++ main.go
@@ -12,12 +12,28 @@ func main() {
 	app.RegisterView(iris.HTML("./web/views", ".html").Reload(true))
 	//app.OnErrorCode(iris.StatusInternalServerError, )
 
+	app.OnErrorCode(iris.StatusInternalServerError, func(ctx iris.Context) {
+		// ctx.Values() 是一个很有用的东西，主要用来使 处理方法与中间件 通信 记住真的很重要
+		// ctx.Values().GetString("error") 获取自定义错误提示信息
+		errMessage := ctx.Values().GetString("error")
+		if errMessage != "" {
+			ctx.Writef("Internal server error: %s", errMessage)
+			return
+		}
+		ctx.Writef("(Unexpected) internal server error")
+	})
+
 	app.Use(func(ctx iris.Context) {
-		ctx.Application().Loger().Info("begin request for path : %s", ctx.Path())
+		//ctx.Application().Loger().Info("begin request for path : %s", ctx.Path())
 		ctx.Next()
 	})
 
 	app.Done(func(ctx iris.Context) {})
 
-	app.Post("")
+	app.Get("/welcome", welcome)
+	app.Run(iris.Addr(":8080"), iris.WithCharset("UTF-8"))
+}
+
+func welcome(ctx iris.Context) {
+	ctx.View("welcome.html")
 }
